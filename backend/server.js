b0dd63fee20d266db949ab88d24f48620f6392f9
Admin <pawneshk22@gmail.com>
2025-11-14 03:47:24 +0530

    added frontend url in cores in server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173',
+  origin: ['https://onemart-ecom.vercel.app',
     'http://localhost:5174'
   ], // Update with your frontend URL
   credentials: true,
