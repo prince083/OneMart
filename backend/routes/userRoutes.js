d0c38aa859b0f306deb48412b8b2f164af513e4d
Admin <pawneshk22@gmail.com>
2026-03-20 04:19:57 +0530

    Critical Fix: Remove invalid GetUserCart import that was crashing backend module loader

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuth } from '../middleware/isAuth.js';
-import { getAdmin, getCurrentUser, GetUserCart } from '../controllers/userController.js';
+import { getAdmin, getCurrentUser } from '../controllers/userController.js';
 import adminAuth from '../middleware/adminAuth.js';
 
 let userRoutes = express.Router();
